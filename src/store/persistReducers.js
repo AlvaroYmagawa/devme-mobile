d1b00cfb599df09c17c6a91c82ab3f9c4506a95e
Kenzo Ymagawa <alvaroymagawa@gmail.com>
2020-08-18 21:14:47 -0300

    change persist name

--- src/store/persistReducers.js
+++ src/store/persistReducers.js
@@ -4,7 +4,7 @@ import { persistReducer } from 'redux-persist';
 export default (reducers) => {
   const persistedReducer = persistReducer(
     {
-      key: 'justParty',
+      key: 'devMe',
       storage: AsyncStorage, // This config is nedeed to mobile env
       blacklist: ['categories', 'posts'], // Do not persist reducers in blackList
     },
